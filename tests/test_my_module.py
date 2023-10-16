0c8b3a64acf213937101e56ccfac8f1048e0a9c2
Yoitsjae <ncubejason896@gmail.com>
2023-10-16 15:02:33 -0700

    update

---
+++ tests/test_my_module.py
@@ -0,0 +1,12 @@
+# test_my_module.py
+
+import unittest
+from my_module import MyClass
+
+class TestMyClass(unittest.TestCase):
+    def test_something(self):
+        # Your test code here
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
